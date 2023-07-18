8cadec5e96f967416fbeeb1fed5561d373e94499
Rahul Mandal <rahulbokaro610@gmail.com>
2023-07-18 15:41:32 +0530

    Advanced Locating Elements

--- hello-selenium/src/main/java/com/ecommerce/test/AdvancedLocatingElements.java
+++ hello-selenium/src/main/java/com/ecommerce/test/AdvancedLocatingElements.java
@@ -36,7 +36,11 @@ public class AdvancedLocatingElements {
 		System.out.println("inputAdminElementsUsingCSSSelectors has " + inputAdminElementsUsingCSSSelectors.size());//3
 		
 		List<WebElement> inputAdminElementsUsingCSSSelectors2 = driver.findElements(By.cssSelector("input[id$='xyz']"));
-		System.out.println("input  whose id's end with xyz " + inputAdminElementsUsingCSSSelectors2.size()); //1
+		System.out.println("input  whose id's end with xyz " + inputAdminElementsUsingCSSSelectors2.size()); //2
+		
+
+		WebElement h4SecondChildInsideDiv = driver.findElement(By.cssSelector("div[id='eCommerce'] h4:nth-child(2)"));
+		System.out.println("h4SecondChildInsideDiv text is " + h4SecondChildInsideDiv.getText()); // h4 second
 		
 	}
 
