8e7d24b2463de3b37376e575f99668f1023a6318
Rahul Mandal <rahulbokaro610@gmail.com>
2023-07-18 15:32:43 +0530

    Advanced Locating Elements

--- hello-selenium/src/main/java/com/ecommerce/test/AdvancedLocatingElements.java
+++ hello-selenium/src/main/java/com/ecommerce/test/AdvancedLocatingElements.java
@@ -11,27 +11,33 @@ public class AdvancedLocatingElements {
 
 	public static void main(String[] args) throws InterruptedException {
 		// Basic configuration
-				WebDriver driver = new ChromeDriver();
-				
-				demoAdvancedXPathCSSSelector(driver);
+		WebDriver driver = new ChromeDriver();
+
+		demoAdvancedXPathCSSSelector(driver);
 
 	}
-	
+
 	static void demoAdvancedXPathCSSSelector(WebDriver driver) {
-		
+
 		String baseUrl = "File:///C:\\Users\\rahul\\eclipse-workspace11\\hello-selenium\\src\\main\\resources\\test.html";
 
-		//String baseUrl = "https://github.com/awtraining1/sl/blob/main/PHASE5/afternoon/hello-selenium/src/main/resources/test.html";
-		
+		// String baseUrl =
+		// "https://github.com/awtraining1/sl/blob/main/PHASE5/afternoon/hello-selenium/src/main/resources/test.html";
+
 		driver.get(baseUrl);
-		
-		
+
 		List<WebElement> inputAdminElements = driver.findElements(By.xpath("//input[contains(@id, 'admin')]"));
 
-		System.out.println("inputAdminElements has " + inputAdminElements.size());		
+		System.out.println("inputAdminElements has " + inputAdminElements.size()); //3
+
+
+		// Same as above but using CSS selectors
+		List<WebElement> inputAdminElementsUsingCSSSelectors = driver.findElements(By.cssSelector("input[id*='admin']"));
+		System.out.println("inputAdminElementsUsingCSSSelectors has " + inputAdminElementsUsingCSSSelectors.size());//3
+		
+		List<WebElement> inputAdminElementsUsingCSSSelectors2 = driver.findElements(By.cssSelector("input[id$='xyz']"));
+		System.out.println("input  whose id's end with xyz " + inputAdminElementsUsingCSSSelectors2.size()); //1
 		
 	}
-	
-	
 
 }
