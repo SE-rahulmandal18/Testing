141b59a448e0d44c10a22f4ba30119fbfa820d58
Rahul Mandal <rahulbokaro610@gmail.com>
2023-07-18 15:09:44 +0530

    selenium

---
+++ hello-selenium/src/main/java/com/ecommerce/test/AdvancedLocatingElements.java
@@ -0,0 +1,16 @@
+package com.ecommerce.test;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class AdvancedLocatingElements {
+
+	public static void main(String[] args) throws InterruptedException {
+		// Basic configuration
+		WebDriver driver = new ChromeDriver();
+
+	}
+	
+	
+
+}
