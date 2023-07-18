75fe33d33ecafb11c9fa940efbd260452b54fbd3
Rahul Mandal <rahulbokaro610@gmail.com>
2023-07-18 17:16:51 +0530

    Process External Elements

---
+++ hello-selenium/src/main/java/com/ecommerce/test/ProcessExternalLocator.java
@@ -0,0 +1,48 @@
+package com.ecommerce.test;
+
+import java.time.Duration;
+
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class ProcessExternalLocator {
+
+	public static void main(String[] args) throws InterruptedException {
+		// Basic configuration
+		WebDriver driver = new ChromeDriver();
+
+		demoExternalElements(driver);
+
+	}
+
+	static void demoExternalElements(WebDriver driver) throws InterruptedException {
+
+		String baseUrl = "File:///C:\\Users\\rahul\\eclipse-workspace11\\hello-selenium\\src\\main\\resources\\test.html";
+		driver.get(baseUrl);
+
+		// driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		Thread.sleep(10000);
+
+		driver.findElement(By.linkText("See an example alert")).click();
+
+		// alert will appear now, may be in 10 secs
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+
+		// Wait for the alert to be displayed
+		wait.until(ExpectedConditions.alertIsPresent());
+
+		// Store the alert in a variable
+		Alert alert = driver.switchTo().alert();
+
+		System.out.printf("\n alert text is %s \n", alert.getText());
+
+		Thread.sleep(10000);
+
+		alert.accept();
+
+	}
+}
