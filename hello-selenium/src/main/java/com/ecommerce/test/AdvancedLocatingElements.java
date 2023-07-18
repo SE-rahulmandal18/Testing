5fb1a1a216fed0c6d49dbeae08b9dd1b2113bcd7
Rahul Mandal <rahulbokaro610@gmail.com>
2023-07-18 16:16:50 +0530

    Advanced Locating Elements

--- hello-selenium/src/main/java/com/ecommerce/test/AdvancedLocatingElements.java
+++ hello-selenium/src/main/java/com/ecommerce/test/AdvancedLocatingElements.java
@@ -6,6 +6,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
 
 public class AdvancedLocatingElements {
 
@@ -28,20 +29,33 @@ public class AdvancedLocatingElements {
 
 		List<WebElement> inputAdminElements = driver.findElements(By.xpath("//input[contains(@id, 'admin')]"));
 
-		System.out.println("inputAdminElements has " + inputAdminElements.size()); //3
-
+		System.out.println("inputAdminElements has " + inputAdminElements.size()); // 3
 
 		// Same as above but using CSS selectors
-		List<WebElement> inputAdminElementsUsingCSSSelectors = driver.findElements(By.cssSelector("input[id*='admin']"));
-		System.out.println("inputAdminElementsUsingCSSSelectors has " + inputAdminElementsUsingCSSSelectors.size());//3
-		
+		List<WebElement> inputAdminElementsUsingCSSSelectors = driver
+				.findElements(By.cssSelector("input[id*='admin']"));
+		System.out.println("inputAdminElementsUsingCSSSelectors has " + inputAdminElementsUsingCSSSelectors.size());// 3
+
 		List<WebElement> inputAdminElementsUsingCSSSelectors2 = driver.findElements(By.cssSelector("input[id$='xyz']"));
-		System.out.println("input  whose id's end with xyz " + inputAdminElementsUsingCSSSelectors2.size()); //2
-		
+		System.out.println("input  whose id's end with xyz " + inputAdminElementsUsingCSSSelectors2.size()); // 2
 
 		WebElement h4SecondChildInsideDiv = driver.findElement(By.cssSelector("div[id='eCommerce'] h4:nth-child(2)"));
 		System.out.println("h4SecondChildInsideDiv text is " + h4SecondChildInsideDiv.getText()); // h4 second
-		
+
+		// Locate and Process multi select list
+		WebElement selectMonthElement = driver.findElement(By.id("month"));
+		Select selectMonth = new Select(selectMonthElement);
+
+		System.out.printf("\n selectMonth.isMultiple() = %s", selectMonth.isMultiple());
+
+		selectMonth.selectByIndex(0);
+		selectMonth.selectByIndex(3);
+		selectMonth.selectByIndex(6);
+
+		List<WebElement> allMonthsSelected = selectMonth.getAllSelectedOptions();
+		for (WebElement monthOption : allMonthsSelected) {
+			System.out.printf("\n option selected = %s", monthOption.getText());
+		}
 	}
 
 }
