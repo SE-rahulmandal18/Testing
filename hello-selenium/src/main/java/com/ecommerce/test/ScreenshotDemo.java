410208016c685a481d17378db698a78c3ee75ff6
Rahul Mandal <rahulbokaro610@gmail.com>
2023-07-19 15:11:42 +0530

    ScreenShortDemo

--- hello-selenium/src/main/java/com/ecommerce/test/ScreenshotDemo.java
+++ hello-selenium/src/main/java/com/ecommerce/test/ScreenshotDemo.java
@@ -1,13 +1,18 @@
 package com.ecommerce.test;
 
+import java.io.File;
 import java.io.IOException;
 
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import com.google.common.io.Files;
+
 public class ScreenshotDemo {
 	
-	public static void main(String[] args) throws InterruptedException, IOException {
+	public static void main(String[] args) throws Exception {
 		// Basic configuration
 		WebDriver driver = new ChromeDriver();
 		
@@ -15,12 +20,20 @@ public class ScreenshotDemo {
 
 	}
 
-	static void demoTakingScreenshot(WebDriver driver) throws InterruptedException {
+	static void demoTakingScreenshot(WebDriver driver) throws IOException, InterruptedException {
 
-		String baseUrl = "File:///C:\\Users\\rahul\\eclipse-workspace11\\hello-selenium\\src\\main\\resources\\test.html";
+        String baseUrl = "File:///C:\\Users\\rahul\\eclipse-workspace11\\hello-selenium\\src\\main\\resources\\test.html";
 	    driver.get(baseUrl);
-	    
-	    
+	  
+		Thread.sleep(2000);
+		
+		TakesScreenshot scrShot = ((TakesScreenshot) driver);
+		
+		File imgTmpFile = scrShot.getScreenshotAs(OutputType.FILE);
+		
+		File destinationFile = new File("D:/tmp/SS.jpg");
+		
+		Files.copy(imgTmpFile, destinationFile);
 	}
 
 }
