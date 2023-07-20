4ace408c78118a155dc4d7a11fe3f0281da410d1
Rahul Mandal <rahulbokaro610@gmail.com>
2023-07-20 15:24:21 +0530

    update

---
+++ automate-web-application/src/main/java/appTest/Login.java
@@ -0,0 +1,27 @@
+package appTest;
+import java.util.concurrent.TimeUnit;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Login {
+	public static void main(String[] args) {
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.facebook.com/");
+		driver.manage().window().maximize();		
+		driver.manage().timeouts().implicitlyWait(10000, TimeUnit.MILLISECONDS);
+	
+		//Add email
+		WebElement UserName = driver.findElement(By.xpath("//input[@id='email']"));
+		UserName.sendKeys("rahul@gmail.com");
+		
+		//Add password
+		WebElement Password = driver.findElement(By.xpath("//input[@id='pass']"));
+		Password.sendKeys("man@123");
+		
+		//Click button to login
+		WebElement LoginBtn = driver.findElement(By.xpath("//button[@name='login']"));
+		LoginBtn.click();
+	}
+}
