7a9cb096da45dce1a01c58510874c4d3a090dc95
Rahul Mandal <rahulbokaro610@gmail.com>
2023-08-25 11:49:52 +0530

    hystrix

---
+++ storeApp-consumer-eureka-ribbon-hystrix/src/main/java/com/example/controller/ProductClientController.java
@@ -0,0 +1,27 @@
+package com.example.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import com.example.entity.Product;
+
+@RestController
+@Scope("request")
+public class ProductClientController {
+
+	@Autowired
+	private RestTemplate restTemplate;
+
+	@GetMapping("/get-products/{id}")
+	public Product getProductById(@PathVariable("id") int id) {
+
+		Product product = restTemplate.getForObject("htttp://product-service/products/" + id, Product.class);
+
+		return product;
+	}
+
+}
