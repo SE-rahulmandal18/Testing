6a51fc0994d53c9440e4bd9de1a60c5044519881
Rahul Mandal <rahulbokaro610@gmail.com>
2023-08-25 12:25:24 +0530

    service layer

--- storeApp-consumer-eureka-ribbon-hystrix/src/main/java/com/example/service/ProductService.java
+++ storeApp-consumer-eureka-ribbon-hystrix/src/main/java/com/example/service/ProductService.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 import com.example.entity.Product;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 
 @Service
 public class ProductService {
@@ -13,10 +14,16 @@ public class ProductService {
 	@Autowired
 	private RestTemplate restTemplate;
 
+	@HystrixCommand(fallbackMethod = "fallbackMethodForGetProductById")
 	public Product getProductById(int id) {
 
 		Product product = restTemplate.getForObject("htttp://product-service/products/" + id, Product.class);
 
 		return product;
+	}
+	
+	public Product fallbackMethodForGetProductById(int id) {
+		
+		return new Product("IFB", 34343.0);
 	}
 }
