27b3a57855ac60d136abf44a8935e885e2f29d06
Rahul Mandal <rahulbokaro610@gmail.com>
2023-08-25 12:02:46 +0530

    contoller

--- storeApp-consumer-eureka-ribbon-hystrix/src/main/java/com/example/controller/ProductClientController.java
+++ storeApp-consumer-eureka-ribbon-hystrix/src/main/java/com/example/controller/ProductClientController.java
@@ -5,23 +5,20 @@ import org.springframework.context.annotation.Scope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 
 import com.example.entity.Product;
+import com.example.service.ProductService;
 
 @RestController
 @Scope("request")
 public class ProductClientController {
 
 	@Autowired
-	private RestTemplate restTemplate;
-
+	private ProductService productService;
+ 	
 	@GetMapping("/get-products/{id}")
-	public Product getProductById(@PathVariable("id") int id) {
-
-		Product product = restTemplate.getForObject("htttp://product-service/products/" + id, Product.class);
-
-		return product;
+ 	public Product getProductById(@PathVariable("id") int id) {
+		
+		return productService.getProductById(id);
 	}
-
 }
