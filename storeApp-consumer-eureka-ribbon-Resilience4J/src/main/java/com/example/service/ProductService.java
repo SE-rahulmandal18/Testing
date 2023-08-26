6fcb4e2950ea5a278c277062c356cb4b9e1b73d0
Rahul Mandal <rahulbokaro610@gmail.com>
2023-08-26 12:00:14 +0530

    implementing Circuit Breaker using Resilience4J on Ribbon Consumer

--- storeApp-consumer-eureka-ribbon-Resilience4J/src/main/java/com/example/service/ProductService.java
+++ storeApp-consumer-eureka-ribbon-Resilience4J/src/main/java/com/example/service/ProductService.java
@@ -11,21 +11,19 @@ import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 @Service
 public class ProductService {
 
-
 	@Autowired
 	private RestTemplate restTemplate;
 
-	@CircuitBreaker(name="product-service", fallbackMethod = "fallbackMethodForGetProductById")
+	@CircuitBreaker(name = "product-service", fallbackMethod = "fallbackMethodForGetProductById")
 	public Product getProductById(int id) {
 
-		Product product = restTemplate.getForObject(
-				"htttp://product-service/products/" + id, Product.class);
+		Product product = restTemplate.getForObject("htttp://product-service/products/" + id, Product.class);
 
 		return product;
 	}
-	
+
 	public Product fallbackMethodForGetProductById(int id, Throwable cause) {
-		
+
 		System.out.println("Exception Raised with the message:===>" + cause.getMessage());
 		return new Product("IFB", 34343.0);
 	}
