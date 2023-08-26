472e244e0ec89435f3df490cf85fbbe611708026
Rahul Mandal <rahulbokaro610@gmail.com>
2023-08-26 14:27:00 +0530

    Implementing Circuit Breaker using Resilience4J on Feign Consumer

--- storeApp-consumer-eureka-Feign-Resilence4J/src/main/java/com/example/proxy/ProductServiceProxy.java
+++ storeApp-consumer-eureka-Feign-Resilence4J/src/main/java/com/example/proxy/ProductServiceProxy.java
@@ -1,5 +1,6 @@
 package com.example.proxy;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.cloud.openfeign.FeignClient;
@@ -8,15 +9,33 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 import com.example.entity.Product;
-import com.example.fallback.ProductServiceFallback;
 
-@FeignClient(name= "product-service", fallback = ProductServiceFallback.class)
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+
+@FeignClient(name= "product-service")
 public interface ProductServiceProxy {
 
-
+    @CircuitBreaker(name= "product-service", fallbackMethod = "fallbackMethodGetAllProducts")
 	@GetMapping(value = "/products", produces = {MediaType.APPLICATION_JSON_VALUE})
 	public List<Product> getAllProducts();
 	
+    @CircuitBreaker(name= "product-service", fallbackMethod = "fallbackMethodGetProductById")
 	@GetMapping(value = "/products/{id}", produces = {MediaType.APPLICATION_JSON_VALUE})
 	public Product getProductById(@PathVariable("id")Integer id);
+
+
+	
+	public default Product fallbackMethodGetProductById(Integer id, Throwable cause) {
+		System.out.println("Exception raised with message:===> " + cause.getMessage());
+		return new Product("IFB", 34343.0);
+		
+	}
+
+
+	public default List<Product> fallbackMethodGetAllProducts(Throwable cause) {
+		System.out.println("Exception raised with message:===> " + cause.getMessage());
+		return new ArrayList<Product>();
+		
+	}
+
 }
