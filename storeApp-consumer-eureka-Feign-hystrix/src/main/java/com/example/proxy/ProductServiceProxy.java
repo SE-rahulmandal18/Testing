8c23c905a0a9c9aa4892ad1b3b5169811fc1ebf3
Rahul Mandal <rahulbokaro610@gmail.com>
2023-08-26 11:52:21 +0530

    circuit breaker with Spring Cloud Hystrix with Feign

---
+++ storeApp-consumer-eureka-Feign-hystrix/src/main/java/com/example/proxy/ProductServiceProxy.java
@@ -0,0 +1,21 @@
+package com.example.proxy;
+
+import java.util.List;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import com.example.entity.Product;
+
+@FeignClient("product-service")
+public interface ProductServiceProxy {
+
+
+	@GetMapping(value = "/products", produces = {MediaType.APPLICATION_JSON_VALUE})
+	public List<Product> getAllProducts();
+	
+	@GetMapping(value = "/products/{id}", produces = {MediaType.APPLICATION_JSON_VALUE})
+	public Product getProductById(@PathVariable("id")Integer id);
+}
