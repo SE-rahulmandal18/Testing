8e5d02a6058173e5c66c1f3b4bf171200e2ac950
Rahul Mandal <rahulbokaro610@gmail.com>
2023-08-27 20:04:28 +0530

    Implementing Circuit Breaker using Resilience4J on Feign Consumer

--- storeApp-consumer-eureka-Feign-Resilence4J/src/main/java/com/example/proxy/ProductServiceProxy.java
+++ storeApp-consumer-eureka-Feign-Resilence4J/src/main/java/com/example/proxy/ProductServiceProxy.java
@@ -11,13 +11,16 @@ import org.springframework.web.bind.annotation.PathVariable;
 import com.example.entity.Product;
 
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
 
 @FeignClient(name= "product-service")
 public interface ProductServiceProxy {
 
+	@Retry(name="product-service")
     @CircuitBreaker(name= "product-service", fallbackMethod = "fallbackMethodGetAllProducts")
 	@GetMapping(value = "/products", produces = {MediaType.APPLICATION_JSON_VALUE})
 	public List<Product> getAllProducts();
+	
 	
     @CircuitBreaker(name= "product-service", fallbackMethod = "fallbackMethodGetProductById")
 	@GetMapping(value = "/products/{id}", produces = {MediaType.APPLICATION_JSON_VALUE})
