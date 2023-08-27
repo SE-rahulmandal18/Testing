12a40027256ca47e8153f88a992ff399f2f8f290
Rahul Mandal <rahulbokaro610@gmail.com>
2023-08-27 22:23:44 +0530

    Zipkin and Sleuth Integration with Ribbon Consumer Application

--- storeApp-consumer-eureka-ribbon-Resilience4J-sz/src/main/java/com/example/StoreAppConsumerEurekaRibbonApplication.java
+++ storeApp-consumer-eureka-ribbon-Resilience4J-sz/src/main/java/com/example/StoreAppConsumerEurekaRibbonApplication.java
@@ -2,14 +2,23 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import brave.sampler.Sampler;
 
 
 @SpringBootApplication
 public class StoreAppConsumerEurekaRibbonApplication {
 
 	public static void main(String[] args) {
-		
 		SpringApplication.run(StoreAppConsumerEurekaRibbonApplication.class, args);
+	}
+	
+	@Bean 
+	public Sampler getSampler() {
+		
+		//return Sampler.create(0.5f);
+		return Sampler.ALWAYS_SAMPLE; 
 	}
 
 }
--- storeApp-consumer-eureka-ribbon-Resilience4J-sz/src/main/java/com/example/controller/ProductClientController.java
+++ storeApp-consumer-eureka-ribbon-Resilience4J-sz/src/main/java/com/example/controller/ProductClientController.java
@@ -1,5 +1,7 @@
 package com.example.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,9 +18,14 @@ public class ProductClientController {
 	@Autowired
 	private ProductService productService;
  	
+	private Logger log = LoggerFactory.getLogger(ProductClientController.class);
+	
 	@GetMapping("/get-products/{id}")
  	public Product getProductById(@PathVariable int id) {
 		
-		return productService.getProductById(id);
+		log.debug("In getProductById with Id:" + id);
+		Product product =  productService.getProductById(id);
+		log.debug("In getProductById with return value Product:" + product);
+		return product;
 	}
 }
