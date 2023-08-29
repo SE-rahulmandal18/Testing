8eccc49a7e25f927e02d02f300a5426f6578f71a
Rahul Mandal <rahulbokaro610@gmail.com>
2023-08-29 09:37:10 +0530

    gateway filterFactory

--- storeApp-cloud-gateway-api/src/main/java/com/example/filters/LoggingGatewayFilterFactory.java
+++ storeApp-cloud-gateway-api/src/main/java/com/example/filters/LoggingGatewayFilterFactory.java
@@ -6,9 +6,6 @@ import org.springframework.cloud.gateway.filter.GatewayFilter;
 import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
 import org.springframework.stereotype.Component;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 import reactor.core.publisher.Mono;
 
 @Component
@@ -26,30 +23,68 @@ public class LoggingGatewayFilterFactory extends AbstractGatewayFilterFactory<Lo
 		return (exchange, chain) -> {
 			
 			//pre-processing
-			if(config.preLogger) {
-				logger.info("Pre GatewayFiler Logging: " + config.baseMessage);
+			if(config.isPreLogger()) {
+				logger.info("Pre GatewayFiler Logging: " + config.getBaseMessage());
 			}
 			
 			return chain.filter(exchange).then(Mono.fromRunnable(() -> {
 				//Post-processing
-				if(config.postLogger) {
-					logger.info("Post GatewayFiler Logging: " + config.baseMessage);
+				if(config.isPostLogger()) {
+					logger.info("Post GatewayFiler Logging: " + config.getBaseMessage());
 				}
 			}));
 		};
 	   
 	}
 	
-	@Data
-	@NoArgsConstructor
-	@AllArgsConstructor
+	
 	public static class Config {
+
+		public Config(String baseMessage, boolean preLogger, boolean postLogger) {
+			super();
+			this.baseMessage = baseMessage;
+			this.preLogger = preLogger;
+			this.postLogger = postLogger;
+		}
+		
+		public Config() {
+			
+		}
 		
 		//Specify your configuration
 		private String baseMessage;
 		private boolean preLogger;
 		private boolean postLogger;
+		public String getBaseMessage() {
+			return baseMessage;
+		}
+		public void setBaseMessage(String baseMessage) {
+			this.baseMessage = baseMessage;
+		}
+		public boolean isPreLogger() {
+			return preLogger;
+		}
+		public void setPreLogger(boolean preLogger) {
+			this.preLogger = preLogger;
+		}
+		public boolean isPostLogger() {
+			return postLogger;
+		}
+		public void setPostLogger(boolean postLogger) {
+			this.postLogger = postLogger;
+		}
+		
+		@Override
+		public String toString() {
+			return "Config [baseMessage=" + baseMessage + ", preLogger=" + preLogger + ", postLogger=" + postLogger
+					+ ", getBaseMessage()=" + getBaseMessage() + ", isPreLogger()=" + isPreLogger()
+					+ ", isPostLogger()=" + isPostLogger() + ", getClass()=" + getClass() + ", hashCode()=" + hashCode()
+					+ ", toString()=" + super.toString() + "]";
+		}
+			
+		
 	}
+	
 
 
 }
