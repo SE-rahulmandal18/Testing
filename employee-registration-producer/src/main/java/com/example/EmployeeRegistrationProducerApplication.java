837a7286d7aacc1b22389d1d3c8c8e860280cb2e
Rahul Mandal <rahulbokaro610@gmail.com>
2023-08-29 11:39:38 +0530

    Spring Cloud Stream –Publish Message to RabbitM

---
+++ employee-registration-producer/src/main/java/com/example/EmployeeRegistrationProducerApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeRegistrationProducerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeRegistrationProducerApplication.class, args);
+	}
+
+}
