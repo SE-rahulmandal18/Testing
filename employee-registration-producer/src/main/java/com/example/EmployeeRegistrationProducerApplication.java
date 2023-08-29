535bec0af66ea86b8dc9bb80b53e09f0280f3cd7
Rahul Mandal <rahulbokaro610@gmail.com>
2023-08-29 20:49:12 +0530

    updated

--- employee-registration-producer/src/main/java/com/example/EmployeeRegistrationProducerApplication.java
+++ employee-registration-producer/src/main/java/com/example/EmployeeRegistrationProducerApplication.java
@@ -2,7 +2,12 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
+@ComponentScan(basePackages = {
+	    "com.example.controller",
+	    "com.example.producer"
+	})
 @SpringBootApplication
 public class EmployeeRegistrationProducerApplication {
 
