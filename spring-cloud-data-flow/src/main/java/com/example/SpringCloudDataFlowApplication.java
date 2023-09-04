abc64d044e101d17efac1eed98802999da02c6bc
Rahul Mandal <rahulbokaro610@gmail.com>
2023-09-04 10:09:10 +0530

    spring-cloud-data-flow

--- spring-cloud-data-flow/src/main/java/com/example/SpringCloudDataFlowApplication.java
+++ spring-cloud-data-flow/src/main/java/com/example/SpringCloudDataFlowApplication.java
@@ -1,13 +1,28 @@
 package com.example;
 
+import java.util.Date;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.messaging.Source;
+import org.springframework.context.annotation.Bean;
+import org.springframework.integration.annotation.InboundChannelAdapter;
+import org.springframework.integration.annotation.Poller;
+import org.springframework.integration.core.MessageSource;
+import org.springframework.messaging.support.MessageBuilder;
 
 @SpringBootApplication
+@EnableBinding(Source.class)
 public class SpringCloudDataFlowApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringCloudDataFlowApplication.class, args);
-	}
+    @Bean
+    @InboundChannelAdapter(value = Source.OUTPUT, poller = @Poller(fixedDelay = "10000", maxMessagesPerPoll = "1"))
+    public MessageSource<Long> timeMessageSource() {
+        return () -> MessageBuilder.withPayload(new Date().getTime()).build();
+    }
 
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudDataFlowApplication.class, args);
+    }
 }
